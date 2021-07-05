8d09fc6ca1ff6dd3bed4bb8d130243484105ce18
IbrahimSheikh <eyebiman@gmail.com>
2021-07-06 00:46:07 +0430

    added Stone & Wall

---
+++ src/main/java/ir/ac/kntu/model/Stone.java
@@ -0,0 +1,9 @@
+package ir.ac.kntu.model;
+
+import javafx.geometry.Point2D;
+
+public class Stone extends GameObject{
+    public Stone(Map map, Point2D pos) {
+        super(map, pos,false);
+    }
+}
---
+++ src/main/java/ir/ac/kntu/model/Wall.java
@@ -0,0 +1,9 @@
+package ir.ac.kntu.model;
+
+import javafx.geometry.Point2D;
+
+public class Wall extends GameObject{
+    Wall(Map map, Point2D pos) {
+        super(map, pos,false);
+    }
+}
