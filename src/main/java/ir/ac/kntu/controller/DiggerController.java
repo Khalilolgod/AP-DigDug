06d65f757dceec7dc06bdcd6d016f7abc1f6e02a
IbrahimSheikh <eyebiman@gmail.com>
2021-07-06 00:48:41 +0430

    added DiggerController

---
+++ src/main/java/ir/ac/kntu/controller/DiggerController.java
@@ -0,0 +1,38 @@
+package ir.ac.kntu.controller;
+
+import ir.ac.kntu.model.Digger;
+import ir.ac.kntu.model.Direction;
+import ir.ac.kntu.model.Map;
+import javafx.scene.input.KeyCode;
+
+public class DiggerController {
+
+    private static DiggerController instance = new DiggerController();
+
+    public static DiggerController getInstance() {
+        return instance;
+    }
+
+    private DiggerController() {
+    }
+
+    public void handlePlayerInputs(KeyCode code){
+        Digger digger = Map.getDigger();
+        switch (code){
+            case UP:
+                digger.move(Direction.UP);
+                break;
+            case RIGHT:
+                digger.move(Direction.RIGHT);
+                break;
+            case DOWN:
+                digger.move(Direction.DOWN);
+                break;
+            case LEFT:
+                digger.move(Direction.LEFT);
+                break;
+            default:
+                break;
+        }
+    }
+}
