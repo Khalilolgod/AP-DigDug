490905f3e9f0a8e91be1ec8425d794abd3dcc8d3
IbrahimSheikh <eyebiman@gmail.com>
2021-07-10 14:37:41 +0430

    added GameObject->getters

--- src/main/java/ir/ac/kntu/model/GameObject.java
+++ src/main/java/ir/ac/kntu/model/GameObject.java
@@ -3,12 +3,20 @@ package ir.ac.kntu.model;
 import javafx.geometry.Point2D;
 
 public abstract class GameObject {
-    Point2D pos;
+    private Point2D pos;
     Map map;
     boolean isCollisionFriendly;
     GameObject(Map map, Point2D pos,boolean isCollisionFriendly){
         this.pos = pos;
         this.map = map;
         this.isCollisionFriendly = isCollisionFriendly;
+    }
+
+    public Point2D getPos() {
+        return pos;
+    }
+
+    public void setPos(Point2D pos) {
+        this.pos = pos;
     }
 }
